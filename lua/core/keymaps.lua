1ce57f8623bb3f59b835bf977dc574088d4ba666
22GNUs <stormaroon@gmail.com>
2023-01-25 15:28:10 +0800

    Add nvimtree plugin

---
+++ lua/core/keymaps.lua
@@ -0,0 +1,9 @@
+local keymap = vim.keymap
+local M = {}
+
+M.nvimtree = {
+  {"<C-n>", "<cmd> NvimTreeToggle <CR>", desc = "toggle nvimtree"},
+  {"<leader>e", "<cmd> NvimTreeFocus <CR>", desc = "focus nvimtree"},
+}
+
+return M
