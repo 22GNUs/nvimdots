154f7467f0b8fbdac7a8e54b753f3ea7bc4190e3
22GNUs <stormaroon@gmail.com>
2023-01-25 18:27:08 +0800

    Update config

--- lua/plugins/mason.lua
+++ lua/plugins/mason.lua
@@ -2,36 +2,36 @@ local ensure_installed = { "lua-language-server" }
 return {
   {
     "williamboman/mason.nvim",
+    name = "mason",
     cmd = { "Mason", "MasonInstall", "MasonInstallAll", "MasonUninstall", "MasonUninstallAll", "MasonLog" },
     lazy = false,
-    config = function()
-      local mason = require("mason")
-      local options = {
-        ensure_installed = ensure_installed,
-        PATH = "prepend",
-        ui = {
-          icons = {
-            package_pending = " ",
-            package_installed = " ",
-            package_uninstalled = " ﮊ",
-          },
-          keymaps = {
-            toggle_server_expand = "<CR>",
-            install_server = "i",
-            update_server = "u",
-            check_server_version = "c",
-            update_all_servers = "U",
-            check_outdated_servers = "C",
-            uninstall_server = "X",
-            cancel_installation = "<C-c>",
-          },
+    opts = {
+      ensure_installed = ensure_installed,
+      PATH = "prepend",
+      ui = {
+        icons = {
+          package_pending = " ",
+          package_installed = " ",
+          package_uninstalled = " ﮊ",
         },
-        max_concurrent_installers = 10,
-      }
+        keymaps = {
+          toggle_server_expand = "<CR>",
+          install_server = "i",
+          update_server = "u",
+          check_server_version = "c",
+          update_all_servers = "U",
+          check_outdated_servers = "C",
+          uninstall_server = "X",
+          cancel_installation = "<C-c>",
+        },
+      },
+      max_concurrent_installers = 10,
+    },
+    config = function(self, opts)
       vim.api.nvim_create_user_command("MasonInstallAll", function()
-        vim.cmd("MasonInstall " .. table.concat(options.ensure_installed, " "))
+        vim.cmd("MasonInstall " .. table.concat(opts.ensure_installed, " "))
       end, {})
-      mason.setup(options)
+      require(self.name).setup(opts)
     end,
   },
 }
