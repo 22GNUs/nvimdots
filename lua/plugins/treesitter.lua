9d50af358b9d47c061e60710fcb78f2fb4e5913a
22GNUs <stormaroon@gmail.com>
2023-01-25 20:37:30 +0800

    Add treesitter

---
+++ lua/plugins/treesitter.lua
@@ -0,0 +1,22 @@
+return {
+  "nvim-treesitter/nvim-treesitter",
+  cmd = { "TSInstall", "TSBufEnable", "TSBufDisable", "TSEnable", "TSDisable", "TSModuleInfo" },
+  event = "VeryLazy",
+  build = ":TSUpdate",
+  opts = {
+    ensure_installed = {
+      "lua",
+    },
+    highlight = {
+      enable = true,
+      use_languagetree = true,
+    },
+    indent = {
+      enable = true,
+    },
+  },
+  config = function(_, opts)
+    local treesitter = require("nvim-treesitter.configs")
+    treesitter.setup(opts)
+  end,
+}
