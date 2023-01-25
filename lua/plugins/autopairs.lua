3694ff0749a1f412622812014af67f300e0df860
22GNUs <stormaroon@gmail.com>
2023-01-25 19:16:37 +0800

    Add autopairs plugin

---
+++ lua/plugins/autopairs.lua
@@ -0,0 +1,18 @@
+return {
+  {
+    "windwp/nvim-autopairs",
+    name = "nvim-autopairs",
+    dependencies = "nvim-cmp",
+    event = "InsertEnter",
+    opts = {
+      fast_wrap = {},
+      disable_filetype = { "TelescopePrompt", "vim" },
+    },
+    config = function(self, opts)
+      require(self.name).setup(opts)
+      local cmp = require("cmp")
+      local cmp_autopairs = require("nvim-autopairs.completion.cmp")
+      cmp.event:on("confirm_done", cmp_autopairs.on_confirm_done())
+    end,
+  },
+}
