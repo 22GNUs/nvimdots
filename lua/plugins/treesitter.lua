715549c77c06fd05ba048ade3d1df3a88e59267e
22GNUs <stormaroon@gmail.com>
2023-01-26 16:42:11 +0800

    Update treesitter setting

--- lua/plugins/treesitter.lua
+++ lua/plugins/treesitter.lua
@@ -1,7 +1,8 @@
+local event = require("core.lazy").event
 return {
   "nvim-treesitter/nvim-treesitter",
   cmd = { "TSInstall", "TSBufEnable", "TSBufDisable", "TSEnable", "TSDisable", "TSModuleInfo" },
-  event = require("core.lazy").event.OnFileOpen,
+  event = event.OnFileOpen,
   build = ":TSUpdate",
   opts = {
     ensure_installed = {
@@ -11,6 +12,7 @@ return {
     highlight = {
       enable = true,
       use_languagetree = true,
+      additional_vim_regex_highlighting = false,
     },
     indent = {
       enable = true,
