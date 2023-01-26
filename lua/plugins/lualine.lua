5fcf3009c857d8f33dc32cf025c5918b9e5368f2
22GNUs <stormaroon@gmail.com>
2023-01-26 13:40:24 +0800

    Lualine style

--- lua/plugins/lualine.lua
+++ lua/plugins/lualine.lua
@@ -1,14 +1,121 @@
-local ui = require("ui.icons").ui
+local colors = {
+  red = "#cdd6f4",
+  grey = "#181825",
+  black = "#1e1e2e",
+  white = "#313244",
+  light_green = "#6c7086",
+  orange = "#fab387",
+  green = "#a6e3a1",
+  blue = "#80A7EA",
+}
+
+local theme = {
+  normal = {
+    a = { fg = colors.black, bg = colors.blue },
+    b = { fg = colors.blue, bg = colors.white },
+    c = { fg = colors.white, bg = colors.black },
+    z = { fg = colors.white, bg = colors.black },
+  },
+  insert = { a = { fg = colors.black, bg = colors.orange } },
+  visual = { a = { fg = colors.black, bg = colors.green } },
+  replace = { a = { fg = colors.black, bg = colors.green } },
+}
+
+local vim_icons = {
+  function()
+    return " "
+  end,
+  separator = { left = "", right = "" },
+  color = { bg = "#313244", fg = "#80A7EA" },
+}
+
+local filename = {
+  "filename",
+  color = { bg = "#80A7EA", fg = "#242735" },
+  separator = { left = "", right = "" },
+}
+
+local filetype = {
+  "filetype",
+  icon_only = true,
+  colored = true,
+  color = { bg = "#313244" },
+  separator = { left = "", right = "" },
+}
+
+local fileformat = {
+  "fileformat",
+  color = { bg = "#b4befe", fg = "#313244" },
+  separator = { left = "", right = "" },
+}
+
+local encoding = {
+  "encoding",
+  color = { bg = "#313244", fg = "#80A7EA" },
+  separator = { left = "", right = "" },
+}
+
+local branch = {
+  "branch",
+  color = { bg = "#a6e3a1", fg = "#313244" },
+  separator = { left = "", right = "" },
+}
+
+local diff = {
+  "diff",
+  color = { bg = "#313244", fg = "#313244" },
+  separator = { left = "", right = "" },
+}
+
+local modes = {
+  "mode",
+  fmt = function(str)
+    return str:sub(1, 1)
+  end,
+  color = { bg = "#fab387		", fg = "#1e1e2e" },
+  separator = { left = "", right = "" },
+}
+
+local function getLspName()
+  local msg = "No Active Lsp"
+  local buf_ft = vim.api.nvim_buf_get_option(0, "filetype")
+  local clients = vim.lsp.get_active_clients()
+  if next(clients) == nil then
+    return msg
+  end
+  for _, client in ipairs(clients) do
+    local filetypes = client.config.filetypes
+    if filetypes and vim.fn.index(filetypes, buf_ft) ~= -1 then
+      return "  " .. client.name
+    end
+  end
+  return "  " .. msg
+end
+
+local dia = {
+  "diagnostics",
+  color = { bg = "#313244", fg = "#80A7EA" },
+  separator = { left = "", right = "" },
+}
+
+local lsp = {
+  function()
+    return getLspName()
+  end,
+  separator = { left = "", right = "" },
+  color = { bg = "#f38ba8", fg = "#1e1e2e" },
+}
+
 return {
   "nvim-lualine/lualine.nvim",
   event = require("core.lazy").event.VeryLazy,
   dependencies = { "nvim-web-devicons", "nvim-lspconfig" },
   opts = {
     options = {
-      theme = "catppuccin",
       icons_enabled = true,
-      component_separators = "|",
-      section_separators = { left = ui.left_separator, right = ui.right_separator },
+      theme = theme,
+      component_separators = { left = "", right = "" },
+      section_separators = { left = "", right = "" },
       disabled_filetypes = {
         statusline = {},
         winbar = {},
@@ -22,25 +129,28 @@ return {
         winbar = 1000,
       },
     },
+
     sections = {
-      lualine_a = { "mode" },
-      lualine_b = { "branch", "diff", "diagnostics" },
-      lualine_c = { "filename" },
-      lualine_x = {
-        "encoding",
-        {
-          "fileformat",
-          icons_enabled = true,
-          symbols = {
-            unix = "LF",
-            dos = "CRLF",
-            mac = "CR",
-          },
-        },
-        "filetype",
+      lualine_a = {
+        --{ 'mode', fmt = function(str) return str:gsub(str, "  ") end },
+        modes,
+        vim_icons,
+        --{ 'mode', fmt = function(str) return str:sub(1, 1) end },
       },
-      lualine_y = { "progress" },
-      lualine_z = { "location" },
+      lualine_b = {
+        filename,
+        filetype,
+        branch,
+        diff,
+      },
+      lualine_x = {
+        encoding,
+        fileformat,
+      },
+      lualine_y = {
+        dia,
+        lsp,
+      },
     },
     inactive_sections = {
       lualine_a = {},
@@ -50,12 +160,12 @@ return {
       lualine_y = {},
       lualine_z = {},
     },
-    tabline = {},
     winbar = {},
     inactive_winbar = {},
     extensions = {
+      "quickfix",
       "nvim-tree",
-      "symbols-outline",
+      "nvim-dap-ui",
       "toggleterm",
     },
   },
