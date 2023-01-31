61e9182b3754f473a0abc10d87248123ce8fec37
22GNUs <stormaroon@gmail.com>
2023-01-31 09:43:10 +0800

    Add markdown to wakatime ft

--- lua/plugins/uitl.lua
+++ lua/plugins/uitl.lua
@@ -1,12 +1,14 @@
 return {
+  -- plenary
   {
     "nvim-lua/plenary.nvim",
     name = "plenary",
     module = true,
   },
+  -- wakatime
   {
     "wakatime/vim-wakatime",
     -- enable just for some filetypes
-    ft = { "lua", "go" },
+    ft = { "lua", "go", "markdown" },
   },
 }
