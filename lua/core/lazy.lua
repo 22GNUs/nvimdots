d76cbee86c41d74fd32d616199f2393ec4e511cb
22GNUs <stormaroon@gmail.com>
2023-02-08 11:13:55 +0800

    Set stable version as default

--- lua/core/lazy.lua
+++ lua/core/lazy.lua
@@ -24,8 +24,8 @@ return {
         lazy = true,
         -- It's recommended to leave version=false for now, since a lot the plugin that support versioning,
         -- have outdated releases, which may break your Neovim install.
-        version = false, -- always use the latest git commit
-        -- version = "*", -- try installing the latest stable version for plugins that support semver
+        -- version = false, -- always use the latest git commit
+        version = "*", -- try installing the latest stable version for plugins that support semver
       },
       install = { colorscheme = { "catppuccin" } },
       checker = {
