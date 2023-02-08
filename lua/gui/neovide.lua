10b7415581c382afe08b3baf2cb95657387f892f
22GNUs <stormaroon@gmail.com>
2023-02-08 16:17:33 +0800

    Update neovide cmdhight config

--- lua/gui/neovide.lua
+++ lua/gui/neovide.lua
@@ -14,7 +14,6 @@ if g.neovide ~= nil then
   -- have bugs when --multigrid enable, disable for now, see https://github.com/neovide/neovide/issues/720
   if g.transparency then
     g.transparency = false
-    o.cmdheight = 0
     -- g.neovide_transparency = 0.0
     -- g.transparency = 0.97
     -- vim.cmd("let g:neovide_background_color = '" .. "#1E1E2E" .. "'.printf('%x', float2nr(255 * g:transparency))")
