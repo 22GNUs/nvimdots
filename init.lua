0140e13dc02217441d956620a89780f4050b583b
22GNUs <stormaroon@gmail.com>
2023-03-14 19:56:04 +0800

    Add keymapping to toggleterm

--- init.lua
+++ init.lua
@@ -1,8 +1,10 @@
 -- be sure to load options first
 require("core.options")
-require("gui")
-require("core.autocmds")
--- bind general keymaps
-require("core.utils").bind_mappings("general")
--- lazy load init
-require("core.lazy").setup()
+if vim.g.vscode == nil then
+  require("gui")
+  require("core.autocmds")
+  -- bind general keymaps
+  require("core.utils").bind_mappings("general")
+  -- lazy load init
+  require("core.lazy").setup()
+end
--- lua/plugins/term.lua
+++ lua/plugins/term.lua
@@ -3,7 +3,8 @@ return {
   "akinsho/toggleterm.nvim",
   keys = require("core.keymaps").toggleterm,
   opts = {
-    open_mapping = false, -- [[<c-\>]],
+    open_mapping = [[<c-\>]],
+    insert_mappings = true, -- whether or not the open mapping applies in insert mode
     hide_numbers = true, -- hide the number column in toggleterm buffers
     shading_factor = "1", -- the degree by which to darken to terminal colour, default: 1 for dark backgrounds, 3 for light
     shade_terminals = true,
@@ -14,7 +15,6 @@ return {
       },
     },
     start_in_insert = true,
-    insert_mappings = true, -- whether or not the open mapping applies in insert mode
     persist_size = true,
     direction = "horizontal",
     close_on_exit = true, -- close the terminal window when the process exits
