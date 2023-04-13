d6b35899860c19256bc123c5d4b8821d8e8ba170
22GNUs <stormaroon@gmail.com>
2023-04-13 23:03:32 +0800

    Update nvimtree setting

--- lua/plugins/editor.lua
+++ lua/plugins/editor.lua
@@ -139,7 +139,6 @@ return {
           adaptive_size = true,
           side = "left",
           width = 25,
-          hide_root_folder = true,
         },
         git = {
           enable = false,
@@ -167,6 +166,7 @@ return {
         renderer = {
           highlight_git = false,
           highlight_opened_files = "none",
+          root_folder_label = false,
 
           indent_markers = {
             enable = false,
